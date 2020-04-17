646d1759257c4e48a6755b6523971020ec2a570b
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-17 16:34:22 +0100

    Extract from keypad to Key element; add animation

---
+++ src/components/Key.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { keypress } from '../actions';
+
+
+function Key(props) {
+
+  const { id, text, action } = props.keyMapping
+  let keyRef;
+
+  // handleKeypress
+  const handleKeypress = (action, text, ref) => {
+    resetAnimation(ref)
+    props.keypress(action, text)
+  }
+
+  //
+  const resetAnimation = (ref) => {
+    ref.classList.remove('bounceIn');
+    setTimeout(() => {
+      ref.classList.add('bounceIn');
+    }, 1);
+  }
+
+  return (
+    < div key={id} className='key-container' >
+      <div
+        id={id}
+        className='key bounceIn'
+        ref={ref => keyRef = ref}
+        onMouseDown={() => handleKeypress(action, text, keyRef)}
+
+      >
+        <p>
+          {text}
+        </p>
+      </div>
+    </div >
+  )
+}
+
+
+export default connect(null, { keypress })(Key);
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './css/Keypad.css';
 import keyMapping from '../mapping/keyMapping';
-import { keypress } from '../actions';
+import Key from './Key';
 
 function KeyPad(props) {
 
 
+  //
   const renderKeys = () => {
     let keys = [];
     for (let i = 0; i < 17; i++) {
-      const { id, text, action } = keyMapping[i];
+      const keyMap = keyMapping[i];
       keys.push(
-        <div key={id} className='key-container'>
-          <div
-            id={id}
-            className='key'
-            onClick={() => props.keypress(action, text)}
-          >
-            <p>
-              {text}
-            </p>
-          </div>
-        </div>
+        <Key
+          keyMapping={keyMap}
+          key={keyMap.id}
+        />
       );
     }
     return keys;
   }
 
+  //
   return (
     <div className='keypad-container'>
       <div className='keys'>
@@ -38,4 +32,5 @@ function KeyPad(props) {
   )
 }
 
-export default connect(null, { keypress })(KeyPad);
+// export default connect(null, { keypress })(KeyPad);
+export default KeyPad;
