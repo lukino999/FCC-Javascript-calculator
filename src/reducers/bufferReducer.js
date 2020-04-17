f87a81e07678e089c98523672d324fc64d96adec
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-17 04:01:32 +0100

    bufferReducer: zero, decimal, clear, digits

--- src/reducers/bufferReducer.js
+++ src/reducers/bufferReducer.js
@@ -20,11 +20,20 @@ import {
 import keyMapping from '../mapping/keyMapping';
 
 const INITIAL_STATE = {
-  buffer: ''
+  buffer: '0',
+  zeroIsAllowed: false,
+  decimalIsAllowed: true
 };
 
 export default (state = INITIAL_STATE, action) => {
+  const { buffer, zeroIsAllowed, decimalIsAllowed } = state;
   switch (action.type) {
+
+    //
+    case CLEAR:
+      return INITIAL_STATE;
+
+    //
     case ONE:
     case TWO:
     case THREE:
@@ -34,10 +43,36 @@ export default (state = INITIAL_STATE, action) => {
     case SEVEN:
     case EIGHT:
     case NINE:
+      let existing = buffer == '0' ? '' : buffer;
       return {
         ...state,
-        buffer: state.buffer + action.text
+        buffer: existing + action.text,
+        zeroIsAllowed: true
       }
+
+    //
+    case ZERO:
+      if (zeroIsAllowed) {
+        if (!buffer) return {
+          ...state,
+          buffer: buffer + action.text,
+          zeroIsAllowed: false
+        }
+        else return {
+          ...state,
+          buffer: buffer + action.text,
+        }
+      }
+
+    //
+    case DECIMAL:
+      if (decimalIsAllowed) return {
+        ...state,
+        buffer: buffer + action.text,
+        decimalIsAllowed: false,
+        zeroIsAllowed: true
+      }
+
 
     default:
       return state;
