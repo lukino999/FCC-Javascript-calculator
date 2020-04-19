41511c94373c3e18f41f6c58728c66f7b2943c4e
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-19 19:07:58 +0100

    UI: add font awesome

--- src/mapping/keyMapping.js
+++ src/mapping/keyMapping.js
@@ -1,21 +1,10 @@
+import React from 'react';
 import {
   CLEAR,
-  SEVEN,
-  EIGHT,
-  NINE,
-  DIVIDE,
-  FOUR,
-  FIVE,
-  SIX,
-  MULTIPLY,
-  ONE,
-  TWO,
-  THREE,
-  SUBTRACT,
-  ZERO,
-  DECIMAL,
-  EQUALS,
-  ADD
+  SEVEN, EIGHT, NINE, DIVIDE,
+  FOUR, FIVE, SIX, MULTIPLY,
+  ONE, TWO, THREE, SUBTRACT,
+  ZERO, DECIMAL, EQUALS, ADD
 } from '../actions/types';
 
 //
@@ -24,19 +13,19 @@ const keyMapping = [
   { id: 'seven', text: '7', action: SEVEN },
   { id: 'eight', text: '8', action: EIGHT },
   { id: 'nine', text: '9', action: NINE },
-  { id: 'divide', text: '÷', action: DIVIDE },
+  { id: 'divide', text: <i className="fas fa-divide op-font-size"></i>, action: DIVIDE },
   { id: 'four', text: '4', action: FOUR },
   { id: 'five', text: '5', action: FIVE },
   { id: 'six', text: '6', action: SIX },
-  { id: 'multiply', text: 'x', action: MULTIPLY },
+  { id: 'multiply', text: <i className="fas fa-times op-font-size"></i>, action: MULTIPLY },
   { id: 'one', text: '1', action: ONE },
   { id: 'two', text: '2', action: TWO },
   { id: 'three', text: '3', action: THREE },
-  { id: 'subtract', text: '-', action: SUBTRACT },
+  { id: 'subtract', text: <i className="fas fa-minus op-font-size"></i>, action: SUBTRACT },
   { id: 'zero', text: '0', action: ZERO },
   { id: 'decimal', text: '.', action: DECIMAL },
   { id: 'equals', text: '=', action: EQUALS },
-  { id: 'add', text: '+', action: ADD }
+  { id: 'add', text: <i className="fas fa-plus op-font-size"></i>, action: ADD }
 ];
 
 export default keyMapping;
